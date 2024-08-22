89587f1d8759aa732f331a0fd80f737717f3f62f
SwitchZer <alfredzagoto21@gmail.com>
2024-08-22 10:43:40 +0700

    Fix Add Transaction

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const customersRoute = require("./customer");
 const accountsRoute = require("./account");
 const depositoRoute = require("./deposito");
+const transactionRoute = require("./transaction");
 
 router.use("/customers", customersRoute);
 router.use("/accounts", accountsRoute);
 router.use("/deposito", depositoRoute);
+router.use("/transactions", transactionRoute);
 
 module.exports = router;
